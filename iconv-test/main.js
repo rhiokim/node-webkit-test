bbd52ea6b88659f7563aeb1dd4084a59d46020e9
rhiokim <rhio.kim@gmail.com>
2014-01-12 15:17:55 +0900

    iconv test

---
+++ iconv-test/main.js
@@ -0,0 +1,9 @@
+//var Iconv = require('iconv').Iconv;
+var fs = require('fs');
+
+fs.readFile('./test.smi', function(err, data) {
+ // var iconv = new Iconv('ISO-2022-JP', 'UTF-8');
+//  var res = iconv.convert(data);
+
+//  document.body.innerHTML = res;
+});
